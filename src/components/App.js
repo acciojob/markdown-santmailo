299eafd00af4b81149193e7b94b595040dfc2001
santmailo <iamsunil006@gmail.com>
2023-09-27 16:41:06 +0530

    solved p coming instead of h1

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,26 @@ import React from "react";
 import "../styles/App.css"
 
 const App = () => {
+    const [textCollection, setTextCollection] = React.useState([]);
 
-    const [text, setText] = React.useState("");
 
     function changeText(e) {
-        setText(e.target.value);
+        const text = e.target.value.split("\n").map(
+            (item)=> {
+                if(item[0]=="#"){
+                    return <h1 className="loading">{item.slice(1)}</h1>
+                }
+                return <p className="loading">{item}</p>
+            }
+        )
+        setTextCollection(text);
     }
-
 
     return (
         <div className="app">
             <textarea className="textarea" onChange={changeText}></textarea>
             <section className="preview">
-                <h1 className="loading">{text}</h1>
+                {textCollection}
             </section>
         </div>
     )
