3df5662fffe9643a27a0e52e79a47cec3d4f35b7
santmailo <iamsunil006@gmail.com>
2023-09-27 16:16:58 +0530

    created the element required and gave class name

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,24 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import "../styles/App.css"
+
+const App = () => {
+
+    const [text, setText] = React.useState("");
+
+    function changeText(e) {
+        setText(e.target.value);
+    }
+
+
+    return (
+        <div className="app">
+            <textarea className="textarea" onChange={changeText}></textarea>
+            <section className="preview">
+                <p className="loading">{text}</p>
+            </section>
+        </div>
+    )
+}
+
+export default App;
 
